089c4601f11813757457427046eba38f704b5a0f
Anas Saad <anaswessam176@gmail.com>
2022-12-29 20:41:36 +0000

    Created functional delete button

--- homepage.js
+++ homepage.js
@@ -1,30 +1,38 @@
+let inputBox = document.getElementById("input");
+let outputDiv = document.getElementById("output");
+
 function displayText() {
-    let inputBox = document.getElementById("input");
-    let outputDiv = document.getElementById("output");
+    const div1 = document.createElement("div");
+    const text1 = document.createElement("p");
+    const dltBtn1 = document.createElement("button");
 
-    const newDiv = document.createElement("div");
-    const newText = document.createElement("p");
-    const newBtn = document.createElement("button");
+    text1.textContent = input.value;
+    text1.classList.add("p-3");
 
-    newText.textContent = input.value;
-    newText.classList.add("p-3");
+    div1.classList.add("accent");
+    div1.classList.add("rounded");
+    div1.classList.add("mt-3");
+    div1.classList.add("animate__animated");
+    div1.classList.add("animate__fadeInDown");
+    childrenCount = outputDiv.childElementCount;
+    div1.setAttribute("id", "textDiv" + (childrenCount + 1));
 
-    newDiv.classList.add("accent");
-    newDiv.classList.add("rounded");
-    newDiv.classList.add("mt-3");
-    newDiv.classList.add("animate__animated");
-    newDiv.classList.add("animate__fadeInDown");
+    dltBtn1.setAttribute("type", "button");
+    dltBtn1.classList.add("btn");
+    dltBtn1.classList.add("btn-secondary");
+    dltBtn1.classList.add("float-right");
+    dltBtn1.innerHTML = "Delete";
+    dltBtn1.setAttribute("onclick", "deleteText(this, " + (childrenCount + 1) + ")");
 
-    newBtn.setAttribute("type", "button");
-    newBtn.classList.add("btn");
-    newBtn.classList.add("btn-secondary");
-    newBtn.classList.add("float-right");
-    newBtn.innerHTML = "Delete";
-    newBtn.setAttribute("onclick", "");
-
-    outputDiv.appendChild(newDiv);
-    newDiv.appendChild(newText);
+    text1.appendChild(dltBtn1);
+    div1.appendChild(text1);
+    outputDiv.appendChild(div1);
 
     //reset inputbox
     inputBox.value = "";
+}
+
+function deleteText(btnstate, divNum) {
+    var removeDiv = document.getElementById("textDiv" + divNum);
+    outputDiv.removeChild(removeDiv);
 }
